2b4884dca8d228654f02107579eec08e815082ff
Bibash Adhikari <bibash1270@gmail.com>
2018-05-08 00:51:07 +0545

    Convert EbikeToken to Mintable token

--- contracts/EbikeToken.sol
+++ contracts/EbikeToken.sol
@@ -1,17 +1,9 @@
 pragma solidity ^0.4.18;
 
-import "../node_modules/zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
-import "../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "../node_modules/zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 
-contract EbikeToken is StandardToken, Ownable {
+contract EbikeToken is MintableToken {
     string public name = "Bike";
     string public symbol = "BIKE";
-    uint8 public decimals = 2;
-    uint public INITIAL_SUPPLY = 21000000;
-
-
-    function EbikeToken() public {
-        totalSupply_ = INITIAL_SUPPLY;
-        balances[msg.sender] = totalSupply_;
-    }
+    uint8 public decimals = 18;
 }
